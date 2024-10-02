6bc5ae33b3279df5606a1e3bf20a33690292b9df
Muhammad Amin <23FTT1111@student.pb.edu.bn>
2024-10-02 18:29:27 +0800

    add a new forgot_password page

---
+++ Python_code/regen.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_random_pass(length=15):
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()-_=+"
+    password = ''.join(random.choice(characters) for i in range (length))
+    return password
+
+new_number = generate_random_pass()
+print(f"Your new password would be: \033[4m{new_number}\033[0m")
