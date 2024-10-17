e2c057e9a11565f1d465d115e35341b01e1d896e
Ameer Danial <23FTT1107@student.pb.edu.bn>
2024-10-17 08:25:06 +0800

    django works now

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login.html', views.login, name='login'),
+    path('registerpage.html', views.register, name='register'),
+    path('Mainmenu.html', views.mainmenu, name='mainmenu'),
+    path('ParentOrChild.html', views.parentorchild, name='parentorchild'),
+    path('Forgot_pass.html', views.forgotpass, name='forgot_password'),
+    path('LanguageTranslator.html', views.langtrans, name='langtrans'),
+    path('profile.html', views.profile, name='profile'),
+    path('Quiz1.html', views.quiz1, name='quiz1'),
+    path('Quiz2.html', views.quiz2, name='quiz2'),
+    path('score1.html', views.score1, name='score1'),
+    path('score2.html', views.score2, name='score2'),
+    path('settings.html', views.settings, name='settings'),
+    path('Trail.html', views.trail, name='trail'), 
+]
---
+++ myproject/myapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'myapp/WelcomePage.html') 
+
+def login(request):
+    return render(request, 'myapp/login.html')
+
+def register(request):
+    return render(request, 'myapp/registerpage.html')
+
+def mainmenu(request):
+    return render(request, 'myapp/Mainmenu.html')
+
+def parentorchild(request):
+    return render(request, 'myapp/ParentOrChild.html')
+
+def forgotpass(request):
+    return render(request, 'myapp/Forgot_pass.html')
+
+def langtrans(request):
+    return render(request, 'myapp/LanguageTranslator.html')
+
+def profile(request):
+    return render(request, 'myapp/profile.html')
+
+def quiz1(request):
+    return render(request, 'myapp/Quiz1.html')
+
+def quiz2(request):
+    return render(request, 'myapp/Quiz2.html')
+
+def score1(request):
+    return render(request, 'myapp/score1.html')
+
+def score2(request):
+    return render(request, 'myapp/score2.html')
+
+def settings(request):
+    return render(request, 'myapp/settings.html')
+
+def trail(request):
+    return render(request, 'myapp/Trail.html')
