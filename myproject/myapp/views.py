53c01771c03c530b7e1ea58c28e3435c0533bbea
Ameer Danial <23FTT1107@student.pb.edu.bn>
2024-11-03 11:17:38 +0800

    testing user authentication

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'myapp/WelcomePage.html') 
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")
     return render(request, 'myapp/login.html')
 
 def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()    
     return render(request, 'myapp/registerpage.html')
 
 def mainmenu(request):
